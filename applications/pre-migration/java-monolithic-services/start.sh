0af79084b49e584f09cba9da1ee1aab500b95b22
JAMES201010 <james.schneider.LL@gmail.com>
2022-09-23 15:45:48 -0500

    JRS commit Sep 23 2020

--- applications/pre-migration/java-monolithic-services/start.sh
+++ applications/pre-migration/java-monolithic-services/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -eux
 
-source ../setenv.sh
+source ../.env/setenv.sh
 
-# rm nohup.out
+if [ -f nohup.out ]; then
+  rm nohup.out
+fi
 
 nohup java -javaagent:/opt/appdynamics/javaagent/ver22.3.0.33637/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent/ver22.3.0.33637 -Dappdynamics.agent.tierName=FinancialServices -Dappdynamics.agent.nodeName=FinancialServices-Node-1 -Dserver.port=8081 -jar AD-Financial-Monolithic-1.0.0.jar &
--- applications/pre-migration/java-webapps/start.sh
+++ applications/pre-migration/java-webapps/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -eux
 
-source ../setenv.sh
+source ../.env/setenv.sh
 
-# rm nohup.out
+if [ -f nohup.out ]; then
+  rm nohup.out
+fi
 
 nohup java -javaagent:/opt/appdynamics/javaagent/ver22.3.0.33637/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent/ver22.3.0.33637 -Dappdynamics.agent.tierName=WebFrontEnd -Dappdynamics.agent.nodeName=WebFrontEnd-Node-1 -Dserver.port=8080 -jar AD-Financial-Web-1.0.0.war &
