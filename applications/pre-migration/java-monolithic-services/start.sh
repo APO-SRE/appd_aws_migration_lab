036182b4445b0d630badf672ce1f7f9b05026d6c
JAMES201010 <james.schneider.LL@gmail.com>
2022-10-03 17:37:59 -0500

    JRS commit Oct 3 2022

--- applications/pre-migration/java-monolithic-services/start.sh
+++ applications/pre-migration/java-monolithic-services/start.sh
@@ -8,4 +8,4 @@ if [ -f nohup.out ]; then
   rm nohup.out
 fi
 
-nohup java -javaagent:/opt/appdynamics/javaagent/ver22.3.0.33637/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent/ver22.3.0.33637 -Dappdynamics.agent.tierName=FinancialServices -Dappdynamics.agent.nodeName=FinancialServices-Node-1 -Dserver.port=8081 -jar AD-Financial-Monolithic-1.0.0.jar &
+nohup java -javaagent:/opt/appdynamics/javaagent/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent -Dappdynamics.agent.tierName=FinancialServices -Dappdynamics.agent.nodeName=FinancialServices-Node-1 -Dserver.port=8081 -jar AD-Financial-Monolithic-1.0.0.jar &
--- applications/pre-migration/java-webapps/start.sh
+++ applications/pre-migration/java-webapps/start.sh
@@ -8,4 +8,4 @@ if [ -f nohup.out ]; then
   rm nohup.out
 fi
 
-nohup java -javaagent:/opt/appdynamics/javaagent/ver22.3.0.33637/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent/ver22.3.0.33637 -Dappdynamics.agent.tierName=WebFrontEnd -Dappdynamics.agent.nodeName=WebFrontEnd-Node-1 -Dserver.port=8080 -jar AD-Financial-Web-1.0.0.war &
+nohup java -javaagent:/opt/appdynamics/javaagent/javaagent.jar -Dappagent.install.dir=/opt/appdynamics/javaagent -Dappdynamics.agent.tierName=WebFrontEnd -Dappdynamics.agent.nodeName=WebFrontEnd-Node-1 -Dserver.port=8080 -jar AD-Financial-Web-1.0.0.war &
